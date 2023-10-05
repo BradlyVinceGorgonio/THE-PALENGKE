23cef4b4c9d66d6114eb5f01857c178acd58fd9f
DanielGals <janjangallaron@gmail.com>
2023-10-06 02:12:01 +0800

    Added Blurr

--- index.js
+++ index.js
@@ -28,3 +28,15 @@ document.addEventListener("click", function(event) {
     }
 });
 
+
+
+
+function toggle()
+{
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+
+}
