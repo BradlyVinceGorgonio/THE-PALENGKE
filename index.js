a0f40d321b8482c260dce6eabc24435cab834b44
DanielGals <janjangallaron@gmail.com>
2023-10-06 22:41:18 +0800

    Checkpoint

--- index.js
+++ index.js
@@ -40,3 +40,14 @@ function toggle()
     popup.classList.toggle('active');
 
 }
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
